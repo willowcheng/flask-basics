564bed96eae4916651c2d68b214815ff12c63b8c
willowcheng <e.wingus@Gmail.com>
2015-03-24 15:07:40 -0400

    Multiple routes

--- flask-basics.py
+++ flask-basics.py
@@ -1,17 +1,26 @@
 from flask import Flask
-from flask import request
 
 app = Flask(__name__)
 
+
 @app.route('/')
+@app.route('/<name>')
 def index(name="Willow"):
-    name = request.args.get('name', name)
     return "Hello from {}".format(name)
+
+
+@app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<float:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
+def add(num1, num2):
+    return '{} + {} = {}'.format(num1, num2, num1 + num2)
+
 
 app.run(debug=True, port=8000, host='127.0.0.1')
 # @app.route('/')
 # def hello_world():
-#     return 'Hello World!'
+# return 'Hello World!'
 #
 #
 # if __name__ == '__main__':
