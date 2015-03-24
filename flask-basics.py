7e8ccc5dc852d5d85433728c27ab70d36981c1be
willowcheng <e.wingus@Gmail.com>
2015-03-24 16:28:39 -0400

    Add templates and static files

--- flask-basics.py
+++ flask-basics.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -6,7 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/<name>')
 def index(name="Willow"):
-    return "Hello from {}".format(name)
+    context = {'name': name}
+    return render_template("index.html", **context)
 
 
 @app.route('/add/<int:num1>/<int:num2>')
@@ -14,7 +16,8 @@ def index(name="Willow"):
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<int:num2>')
 def add(num1, num2):
-    return '{} + {} = {}'.format(num1, num2, num1 + num2)
+    context = {'num1': num1, 'num2': num2}
+    return render_template("add.html", **context)
 
 
 app.run(debug=True, port=8000, host='127.0.0.1')
---
+++ static/scripts.js
@@ -0,0 +1,4 @@
+/**
+ * Created by willowcheng on 3/24/2015.
+ */
+alert("Howdy!");
